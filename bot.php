62972715e7246c184df0a0541b2a4ba6c0c79a6c
papon78 <roypapon103@gmail.com>
2024-11-17 02:08:40 +0600

    Update bot.php

--- bot.php
+++ bot.php
@@ -10,7 +10,7 @@ if($message == "/start") {
 }
 
 function sendMessage($chat_id, $message) {
-    $apiToken = "7930320812:AAH264MiuNQHIF7K18lb7Cf7R2eZlfKK1fw";
+    $apiToken = "7833886974:AAGZHWqSA2x83WaDx-Adjf20hOjFHrex_sk";
     $url = "https://api.telegram.org/bot$apiToken/sendMessage?chat_id=$chat_id&text=".urlencode($message);
     file_get_contents($url);
 }
